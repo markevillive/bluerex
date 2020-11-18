a92297bfc39e2977e0a421171063188a7512662b
Marc Shreder <mark_evillive@hotmail.com>
2020-11-19 02:02:28 +0700

    Lesson 19

--- README.md
+++ README.md
@@ -239,11 +239,11 @@ function bluerex_get_background($field, $cat = null, $cover = true){
 
 ## Lesson 12 Section Lets
 
-# lesson 13 Section Design
+## lesson 13 Section Design
 
-# lesson 14 Gallery 
+## lesson 14 Gallery 
 
-# Lesson 15 Custom post types
+## Lesson 15 Custom post types
 - Добавлена кастомная функция `bluerex_reviews`, добавляющая тип записи "Отзывы"
 
 ```php
@@ -274,12 +274,12 @@ function bluerex_reviews(){
 - Созданы дополнительные поля для этого типа записей.
 - Добавлено 3 записи этого типа.
 
-# Section Reviews
+## Section Reviews
 
 - Complete.
-# Section Social
+## Section Social
 - Complete
-# Section Form
+## Section Form
 
 ## Plugin "Contact Form 7"
 
@@ -287,7 +287,10 @@ function bluerex_reviews(){
 - Создана форма 
 - Coздана страница "Contact" с шорткодом этой формы.
 
-- 
+## Lesson 19
+
+## Lesson 20
+
 
 
 
--- functions.php
+++ functions.php
@@ -124,16 +124,28 @@ add_action( 'after_setup_theme', 'bluerex_content_width', 0 );
 function bluerex_widgets_init() {
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'bluerex' ),
-			'id'            => 'sidebar-1',
+			'name'          => esc_html__( 'Footer Left', 'bluerex' ),
+			'id'            => 'sidebar-footer-1',
 			'description'   => esc_html__( 'Add widgets here.', 'bluerex' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
+			'before_widget' => '<div id="%1$s" class="widget %2$s col-6">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h5 >',
+			'after_title'   => '</h5>',
 		)
 	);
+    register_sidebar(
+        array(
+            'name'          => esc_html__( 'Footer Right', 'bluerex' ),
+            'id'            => 'sidebar-footer-2',
+            'description'   => esc_html__( 'Add widgets here.', 'bluerex' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s ">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h5 >',
+            'after_title'   => '</h5>',
+        )
+    );
 }
+
 add_action( 'widgets_init', 'bluerex_widgets_init' );
 
 /**
